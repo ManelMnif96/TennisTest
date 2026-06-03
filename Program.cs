118611d2274416c1734b6cd44c789b75b15f0445
unknown <manelmnif96@gmail.com>
2026-06-03 14:03:36 +0200

    Fix program.cs

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ builder.Services.AddSingleton<PlayerService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.WebHost.UseUrls("http://0.0.0.0:10000");
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
